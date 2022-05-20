38d2cea7deadb9180f75ce659bf7c7bb5adc76b0
BTC Oracle <carlhamilton1020@gmail.com>
2022-05-20 12:14:00 +1000

    contracts/Yearn.sol update - added 1 lines

--- contracts/Yearn.sol
+++ contracts/Yearn.sol
@@ -1,1 +1,12 @@
 // SPDX-License-Identifier: MIT
+/**
+ * Created on 2021-02-11
+ * @summary: Jibrel Aave Tranche Protocol
+ * @author: Jibrel Team
+ */
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
